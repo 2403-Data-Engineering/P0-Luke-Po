73c7fa69d522d341956c2c98f7640876d677a6de
shoti411 <lcposhoti411@gmail.com>
2026-04-02 16:23:07 -0400

    class to course in courseService

--- src/Service/CourseService.py
+++ src/Service/CourseService.py
@@ -1,33 +1,33 @@
-from DAO.CourseDAO import ClassDAO
+from DAO.CourseDAO import CourseDAO
 from Model.Course import Course
 class CourseService:
-    """Service layer for business logic related to Class operations."""
+    """Service layer for business logic related to Course operations."""
     
-    def __init__(self, class_dao: ClassDAO):
+    def __init__(self, course_dao: CourseDAO):
         """
-        Initialize the service with a class_dao.
+        Initialize the service with a course_dao.
         
         Args:
-            class_dao: Data access object for Class entities
+            course_dao: Data access object for Course entities
         """
-        self.class_dao = class_dao
+        self.course_dao = course_dao
     
     def get_course_by_id(self, course_id: int) -> Course:
         """Retrieve a course by ID."""
-        return self.class_dao.get_by_id(course_id)
+        return self.course_dao.get_course_by_id(course_id)
     
     def create_course(self, course_data: Course) -> Course:
         """Create a new course."""
-        return self.class_dao.create_class(course_data)
+        return self.course_dao.create_course(course_data)
     
     def update_course(self, course_id: int, course_data: Course) -> None:
         """Update an existing course by its ID"""
-        return self.class_dao.update_class(course_id, course_data)
+        return self.course_dao.update_course(course_id, course_data)
     
     def delete_course(self, course_id: int) -> None:
         """Delete a course by ID."""
-        return self.class_dao.delete_class(course_id)
+        return self.course_dao.delete_course(course_id)
     
     def get_all_courses(self) -> list[Course]:
         """Retrieve all courses."""
-        return self.class_dao.get_all_classes()
+        return self.course_dao.get_all_courses()
