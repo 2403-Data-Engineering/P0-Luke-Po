06f1b59f027b01732765caee5d4363e0329efb9a
shoti411 <lcposhoti411@gmail.com>
2026-04-03 12:56:14 -0400

    partial specific course render

--- src/Controller/Menus/CourseMenu.py
+++ src/Controller/Menus/CourseMenu.py
@@ -33,13 +33,14 @@ class CourseMenu(Menu):
                 self.controller.navigate(CourseMenu(self.controller))
             case '2':
                 print("Creating a Course...")
-                print("Enter a the course you want to create in the format required:")
-                print("Course Name, Professor ID")
+                print("Enter a the course you want to create in the format required (JSON):")
+                print("""{"id": 0, "name": "Dummy Class", "students": [{"id": 0, "first_name": "Dummy", "last_name": "Student", "year": 0, "major": "Physics", "email": "dummys@gmail.com"}], "professor": {"id": 0, "first_name": "Dummy", "last_name": "Professor", "department": "Department", "email": "dummyprofessor@gmail.com"}}""")
                 
-                course_input = input().split(",")
+                course_input = input()
                 
-                #TODO: Still need to check whether professor id is valid
-                self.course_controller.create_course(Course(0, course_input[0], [], Professor(int(course_input[1]))))
+                
+                #TODO: Still need to check whether every id is valid and everything is valid
+                self.course_controller.create_course(ParseJSON.parse_course(course_input))
                 
                 # try:
                 #     self.course_controller.create_course(Course(course_input[0], course_input[1], int(course_input[2])))
@@ -56,24 +57,43 @@ class CourseMenu(Menu):
                 course_id_input = input()
                 
                 c = self.course_controller.get_course_by_id(int(course_id_input))
-                print(c)
+                c.print_course()
                 self.controller.navigate(CourseMenu(self.controller))
             case '4':
                 print("Updating a Course...")
-                print("Enter the course you want to update in the format required:")
-                print("Course ID, Course Name, Professor ID")
-                print("EX: 0, Intro to Computer Science, 0")
+                print("Enter the course you want to update in the format required (JSON):")
+                print("""{"id": 0, "name": "Dummy Class", "students": [{"id": 0, "first_name": "Dummy", "last_name": "Student", "year": 0, "major": "Physics", "email": "dummys@gmail.com"}], "professor": {"id": 0, "first_name": "Dummy", "last_name": "Professor", "department": "Department", "email": "dummyprofessor@gmail.com"}}""")
 
+            #only need id for deletion
             case '5':
                 print("Deleting a Course...")
                 print("Enter the course id of the course you want to delete:")
                 print("Course ID")
                 course_input = input()
-                self.course_controller.get_course_service().delete_course(ParseJSON.parse_course(course_input))
+                self.course_controller.get_course_service().delete_course(int(course_input))
                 self.controller.navigate(CourseMenu(self.controller))
             case '6':
                 self.controller.navigate(MainMenu(self.controller))
             case _:
                 print("Invalid choice, please try again.")
+                self.controller.navigate(CourseMenu(self.controller))
+                
+    def render_course(self) -> None:
+        print("Welcome to the Specific Course Menu. Please select an option:")
+        print("1. Add a student to this course")
+        print("2. Remove a Student from this course")
+        print("3. Go Back to the Main Menu")
+        user_input = input().lower
+        match user_input:
+            case '1':
+                #TODO
+                pass
+            case '2':
+                #TODO
+                pass
+            case '3':
+                self.controller.navigate(MainMenu(self.controller))
+            case _:
+                print("Invalid choice, please try again")
                 self.controller.navigate(CourseMenu(self.controller))
         
