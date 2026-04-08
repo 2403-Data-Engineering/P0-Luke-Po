a227611a58dd3a8bfe5b8e068a5ec55238a90186
shoti411 <lcposhoti411@gmail.com>
2026-04-08 15:06:12 -0400

    fix for creating student

--- src/Model/ParseJSON.py
+++ src/Model/ParseJSON.py
@@ -8,13 +8,13 @@ class ParseJSON:
     @staticmethod
     def parse_professor(json_str: str) -> Professor:
         data = json.loads(json_str)
-        return Professor(id=data.get("id", 0), first_name=data.get("firstname", "Dummy"), last_name=data.get("lastname", "Professor"), department=data.get("department", "Dummy Department"), email=data.get("email", "Dummy@email.com"))
+        return Professor(id=data.get("id", 0), first_name=data.get("first_name", "Dummy"), last_name=data.get("last_name", "Professor"), department=data.get("department", "Dummy Department"), email=data.get("email", "Dummy@email.com"))
     
     
     @staticmethod
     def parse_student(json_str: str) -> Student:
         data = json.loads(json_str)
-        return Student(id=data.get("id", 0), first_name=data.get("firstname", "Dummy"), last_name=data.get("lastname", "Student"), year=data.get("year", 0), major=data.get("major", "Dummy Major"), email=data.get("email", "Dummy@email.com"))
+        return Student(id=data.get("id", 0), first_name=data.get("first_name", "Dummy"), last_name=data.get("last_name", "Student"), year=data.get("year", 0), major=data.get("major", "Dummy Major"), email=data.get("email", "Dummy@email.com"))
     
     @staticmethod
     def parse_course(json_str: str) -> Course:
